8965274a3b662d7d9b18c827d812207c3d36ff87
kevohm <kevinkbet@gmail.com>
2025-02-20 09:41:47 +0300

    feat: added dummy admin data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create!(
+  email_address: 'admin@example.com',
+  password: 'securepassword',
+  password_confirmation: 'securepassword',
+  role: 1
+)
