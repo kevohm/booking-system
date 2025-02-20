bea25d7f57b9c8dfcf47e195f337ed35dfaa52e5
kevohm <kevinkbet@gmail.com>
2025-02-20 10:20:08 +0300

    fix: change to /tmp

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,17 +6,17 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-# Ensure the database directory exists on the persistent disk
-mkdir -p /var/data/db
+# Ensure the database directory exists in /tmp
+mkdir -p /tmp/sqlite
 
 # Check if the database already exists; if not, create it
-if [ ! -f /var/data/db/production.sqlite3 ]; then
+if [ ! -f /tmp/sqlite/production.sqlite3 ]; then
   echo "Database not found. Creating a new one..."
   bundle exec rails db:create
 fi
 
 # Symlink the database to the Rails app's default location
-ln -sf /var/data/db/production.sqlite3 db/production.sqlite3
+ln -sf /tmp/sqlite/production.sqlite3 db/production.sqlite3
 
 # Run database migrations
 bundle exec rails db:migrate:status
